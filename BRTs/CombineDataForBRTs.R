b05f8d9bd79b79bf0fc3026483b712f61fd8e146
Johanna Wren <johanna.wren@noaa.gov>
2025-11-18 08:58:42 -1000

    removed redundancy in code and added a line to save the resulting file

--- BRTs/CombineDataForBRTs.R
+++ BRTs/CombineDataForBRTs.R
@@ -28,7 +28,7 @@ setwd(file.path(mainDir, 'FisheryData/BRT data'))
 ncfiles <- list.files(pattern='nc')
 # read in all files into a list
 dataList <- lapply(ncfiles, function(x) tidync(x) %>% hyper_tibble() %>% mutate_if(is.character, as.numeric))
-# Fix colnames in the oxygen data
+# Change colnames in the oxygen data to be consistent with the other files
 colnames(dataList[[2]]) <- c('Depth at Oxygen 2mLperL', colnames(dataList[[1]])[2:4])
 # change month indexing in oxygen data to be consistent with all other datasets
 # start with month=1 for Jan 1995. O2 starts with month=1 Jan 1993
@@ -40,9 +40,9 @@ dataDF <- dataList %>%
 
 #------------------------------------------------
 ## Climate indices
-# Function to clean up and make the climate indicies consistent
-ClimVarClean <- function(ClimIndex, MonName, YrName) {
-  idx <- ClimIndex %>% 
+# Function to clean up and make the climate indices consistent
+ClimVarClean <- function(ClimIndexFile, MonName, YrName) {
+  idx <- read.csv(ClimIndexFile) %>% 
     rename(Mon=MonName, Yr=YrName) %>% 
     filter(Yr >= 1995, Yr <= 2024) %>%
     arrange(Yr, Mon) 
@@ -51,20 +51,11 @@ ClimVarClean <- function(ClimIndex, MonName, YrName) {
   return(idx)
 }
 
-# Read in raw data
-npgoRaw <- read.csv('NPGO.csv')
-head(npgoRaw)
-meiRaw <- read.csv('MEIv2.csv')
-head(meiRaw)
-oniRaw <- read.csv('ONI_withPhases.csv')
-head(oniRaw)
-pdoRaw <- read.csv('PDO.csv')
-head(pdoRaw)
 # Apply function to climate index data
-npgo <- ClimVarClean(npgoRaw, MonName = 'MONTH', YrName = 'YEAR')
-mei <- ClimVarClean(meiRaw, MonName = 'SecondMonth', YrName = 'Year')
-oni <- ClimVarClean(oniRaw, MonName = 'CentralMonth', YrName = 'YR')
-pdo <- ClimVarClean(pdoRaw, MonName = 'Month', YrName = 'Year')
+npgo <- ClimVarClean('NPGO.csv', MonName = 'MONTH', YrName = 'YEAR')
+mei <- ClimVarClean('MEIv2.csv', MonName = 'SecondMonth', YrName = 'Year')
+oni <- ClimVarClean('ONI_withPhases.csv', MonName = 'CentralMonth', YrName = 'YR')
+pdo <- ClimVarClean('PDO.csv', MonName = 'Month', YrName = 'Year')
 # Combine all climate index data into one dataframe (wide format)
 climIdx <- list(pdo, npgo, oni, mei) %>% 
   reduce(full_join, by=c('Month', 'Yr', 'Mon'))
@@ -76,14 +67,22 @@ allDat <- dataDF %>%
   rename(MonthIndex=Month, Year=Yr, Month=Mon) %>% 
   arrange(MonthIndex) %>% 
   select(Year, Month, MonthIndex, Longitude, Latitude, everything())
-
+  
 #------------------------------------------------
 # Add bait and leader material
 # Bait
 # bait is sanma/sardine until Dec 2020, then a transition period Jan 2021 - Dec 2022, and milkfish starting Jan 2023
 allDat <- allDat %>% 
-  mutate(Bait=if_else(Year <= 2020, 'Sanma/Sardine', if_else(Year <= 2022, 'Transition', 'Milkfish')))
+  mutate(Bait=if_else(Year <= 2020, 'SanmaSardine', if_else(Year <= 2022, 'Transition', 'Milkfish')))
+# Crude plot to make sure the data looks correct
+ggplot(allDat, aes(MonthIndex, Bait)) + geom_path()
 # Leader
-# leader material is poorly reported prior to Jul 2000 (month 307), is wire Aug 2000 (308) - May 2021 (317), and mono starting in Jun 2021 (318)
+# leader material is poorly reported prior to Jul 2000 (month 67), is wire Aug 2000 (308) - May 2021 (317), and mono starting in Jun 2021 (318)
 allDat <- allDat %>% 
-  mutate(Leader=if_else(MonthIndex <= 307, 'PoorlyReported', if_else(MonthIndex >= 318, 'Mono', 'Wire')))
+  mutate(Leader=if_else(MonthIndex <= 67, 'PoorlyReported', if_else(MonthIndex >= 318, 'Mono', 'Wire')))
+# Crude plot to make sure the data looks correct
+ggplot(allDat, aes(MonthIndex, Leader)) + geom_path()
+
+#------------------------------------------------
+# Save file
+saveRDS(allDat, 'ENSO_BRT_data.rds')
