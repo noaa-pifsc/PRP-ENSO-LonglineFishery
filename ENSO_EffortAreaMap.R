8e4d232f400f613d637c4f57b9d55f097152a349
Johanna Wren <johanna.wren@noaa.gov>
2026-03-02 12:08:15 -1000

    one last unsuccessful try at plotting on projection other than mercator

--- ENSO_EffortAreaMap.R
+++ ENSO_EffortAreaMap.R
@@ -38,9 +38,33 @@ plot(effortRast)
 writeCDF(effortRast, 'FisheryData/BRT data/ENSO_EffortRaster.nc')
 
 ### Can't figure out how to make a plot look nice in ggplot with a projection other than mercator. So I'm just saving the effort raster and plotting it in pyGMT
-# # change projection on the raster
-# effortMoll <- project(effortRast, '+proj=moll +lon_0=200')
+# # # change projection on the raster
+# effortMoll <- project(effortRast, '+proj=moll +lon_0=210')
 # 
+# # get extent from lon lat to m
+# lon_min <- 160; lon_max <- 260
+# lat_min <- -2; lat_max <- 45
+# # Create an extent object and project it to Mollweide
+# e <- ext(lon_min, lon_max, lat_min, lat_max)
+# e_sfc <- as.polygons(e, crs="+proj=longlat +datum=WGS84")
+# moll_proj <- "+proj=moll +lon_0=210"
+# e_moll <- project(e_sfc, moll_proj)
+# new_ext <- ext(e_moll)
+# 
+# world <-ne_countries(scale = 'medium', returnclass = 'sf', continent = 'north america') %>%
+#   select(1)
+# worldcrop <- #st_crop(world, e) %>%
+#   st_transform(world, '+proj=moll +lon_0=210')
+# 
+# ggplot() +
+#   geom_spatraster(data = effortMoll) +
+#   geom_sf(data=worldcrop) + 
+#   scale_fill_viridis_c(na.value = 'transparent') +
+#   coord_sf(crs = "+proj=moll +lon_0=220", datum = sf::st_crs(4326), xlim=new_ext[1:2], ylim=new_ext[3:4], expand = F) +
+#   theme_minimal() + 
+#   theme(panel.border = element_rect(color='black'))
+# 
+# # 
 # # make basemap
 # world <-ne_countries(scale = 'medium', returnclass = 'sf') %>%
 #   select(1) #%>%
@@ -62,3 +86,36 @@ writeCDF(effortRast, 'FisheryData/BRT data/ENSO_EffortRaster.nc')
 #         legend.key.width = unit(0.5, 'cm'),
 #         legend.margin = margin(0, 0, 0, 0),
 #         legend.text = element_text(size = 10))
+
+# # plotting R different projections (https://www.r-bloggers.com/2019/04/zooming-in-on-maps-with-sf-and-ggplot2/)
+# 
+# 
+# # Source - https://stackoverflow.com/a/74566254
+# # Posted by Robert Hijmans, modified by community. See post 'Timeline' for change history
+# # Retrieved 2026-02-27, License - CC BY-SA 4.0
+# 
+# library(ggplot2)
+# p <- project(effortRast, method="near", "+proj=moll +lon_0=-140", mask=TRUE)
+# land <- ne_countries(scale='medium', returnclass='sf')
+# ggplot() +
+#   geom_spatraster(data=p) +
+#   geom_sf(data=land, fill='transparent') +
+#   coord_sf(xlim = c(-180,-110), ylim=c(-5,45)) +
+#   scale_fill_viridis_c(na.value='transparent') +
+#   scale_x_continuous(breaks=seq(-180,-110,by=10))
+# 
+# 
+# # Define a bounding box in WGS84 (lon/lat)
+# bbox_area <- st_bbox(c(xmin = -20, xmax = 45, ymin = 30, ymax = 73), crs = st_crs(4326))
+# 
+# # Crop the original WGS84 data
+# world_cropped <- st_crop(ne_countries(scale = "medium", returnclass = "sf"), bbox_area)
+# 
+# # Plot the cropped data and use coord_sf to apply the Mollweide projection
+# ggplot(data = world_cropped) +
+#   geom_sf() +
+#   coord_sf(crs = "+proj=moll") # Projection applied here
+# 
+# ggplot() +
+#   geom_spatraster(data=p) +
+#   scale_fill_viridis_c(na.value = 'transparent')
